2728e3ca876b24a3bfada28252f21ab95d6b1232
Matheus Matias <mmatias@MacBook-Air-de-Matheus.local>
2023-03-06 17:41:53 -0300

    commit 5

--- app.js
+++ app.js
@@ -7,14 +7,34 @@ const express = require('express');
 const app = express();
 // Morgan - Realiza monitoramento de requisicoes
 const morgan = require('morgan');
+// BodyParser - módulo capaz de converter o body da requisição para vários formatos.
+const bodyParser = require('body-parser');
 // Defini a rota de produtos
-const productRoute = require('./routes/products')
+const productRoute = require('./routes/products');
 
 //Rotas das informacoes
 app.use(morgan('dev'));
+//Setuo do body como sendo para apenas dados simples
+app.use(bodyParser.urlencoded({extended: false}));
+//Em formato JSON
+app.use(bodyParser.json());
+
 //Defini como será o nome da rota, e "chama a referencia"
 app.use('/products',productRoute);
 
+//Tratamento de CORS - Permitindo o acesso de todos, só em JSON
+app.use((req,res,next)=> {
+    res.header('Acces-Control-Allow-Origin','*');
+    res.header('Acces-Control-Allow-Header',
+    'Origin,X-Requrested-With, Content-Type,Accept, Authorization');
+
+    if (req.method === 'OPTIONS'){
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({})
+    }
+
+    next();
+});
 
 //Tratamento de erros, caso seja digitado uma rota inexistente
 app.use((req,res,next) => {
--- routes/products.js
+++ routes/products.js
@@ -2,30 +2,102 @@
 
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 // Retorna todos os produtos
 router.get('/', (req,res,next) => {
-    res.status(200).send({
-        mensagem:'Usando o get dentro da rota de produtos'
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM products',
+            (error, result ,field) => {
+
+                if(error){
+                    return res.status(500).send ({
+                        error: error,
+                        response: null
+                    });
+                }
+
+                return res.status(200).send({
+                    result: result,
+                });
+
+            }
+        )
     });
 });
 
 
 router.get('/:id_product', (req,res,next) => {
-    //Recupera o valor de :id_product
-    const id = req.params.id_product 
-    if (id === '1234'){
-        res.status(200).send({
-            mensagem:'Produto encontrado', 
-            id: id
-        });
-    } else {
-        res.status(200).send({
-            mensagem:'ID nao encontrado.', 
-            
-        });
-    }
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT id_product FROM products',
+            (error, result ,field) => {
+
+                if(error){
+                    return res.status(500).send ({
+                        error: error,
+                        response: null
+                    });
+                }
+
+                return res.status(200).send({
+                    result: result,
+                });
+
+            }
+        )
+    });
+});
+
+// Para cadastro de produtos
+router.post('/', (req, res, next) => {
+    
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'insert into products (id_product,name,price,imageName,description,measurementChart,size,category,itsNew,itsTopProduct) VALUES (?,?,?,?,?,?,?,?,?,?)',
+            [req.body.id_product,req.body.name,req.body.price,req.body.imageName,req.body.description,req.body.measurementChart,req.body.size,req.body.category,req.body.itsNew,req.body.itsTopProduct],
+            (error, result ,field) => {
+                //Método importante - Realiza um release do pull, evitando congestionamento nas chamadas
+                conn.release();
+
+                if(error){
+                    return res.status(500).send ({
+                        error: error,
+                        response: null
+                    });
+                }
+
+                res.status(201).send({
+                    mensagem: 'Inserido um novo produto com sucesso',
+                    id_product: result.insertId
+                });
+
+            }
+        )
+    });
+
 });
 
 
+
 module.exports = router;
+
+
+
+
+//const product = {
+    //     id: req.body.id ,
+    //     name: req.body.name ,
+    //     price: req.body.price ,
+    //     imageName: req.body.imageName ,
+    //     description: req.body.description ,
+    //     measurementChart: req.body.measurementChart ,
+    //     size: req.body.size ,
+    //     category: req.body.category ,
+    //     itsNew: req.body.itsNew ,
+    //     itsTopProduct: req.body.itsTopProduct ,
+    // }
